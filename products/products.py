8500a18ef5e564dd0458a1d0eae7f92ad34a08f1
karinchic301 <kar_rash@mail.ru>
2025-06-19 18:13:43 +0300

    Add basic products module with add/list functions

---
+++ products/products.py
@@ -0,0 +1,9 @@
+# products/products.py
+
+products_catalog = []
+
+def add_product(name, price):
+    products_catalog.append({"name": name, "price": price})
+
+def list_products():
+    return products_catalog
